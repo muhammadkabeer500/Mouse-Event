de9d9bbb56083b54c4ecbf9d5506d5581dcdb0c6
Kabeersheikh005 <kabeersheikh008@gmail.com>
2024-06-22 09:35:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const newImage = document.querySelector("#image1")
+const newImage2 = document.querySelector("#image2")
+
+
+// first card //
+function setNewImage() {
+    newImage.src = "";
+}
+function setOldImage() {
+    newImage.src = ""
+}
+
+// second card //
+function setImage() {
+    newImage2.src = "";
+}
+function setOutImage() {
+    newImage2.src = ""
+}
